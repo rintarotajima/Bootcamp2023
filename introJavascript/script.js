b224d9f8ce746a110c5be85ee9c067df8c0c2b9f
rintarotajima <rintarotajima@gmail.com>
2024-04-08 18:29:02 +0900

    [add] else if

--- introJavascript/script.js
+++ introJavascript/script.js
@@ -1,2 +1,15 @@
 console.log("Javascriptの世界へようこそ");
 console.log("Javascriptの冒険へ行くぞ");
+
+const age = 25;
+
+if (age < 5) {
+    console.log("無料で映画をご覧になれます");
+} else if (age < 12) {
+    console.log("500円で映画をご覧になれます");
+} else if (age < 15) {
+    console.log("700円で映画をご覧になれます");
+} else if (age >= 20) {
+    console.log("映画を見ることはできません");
+}
+
