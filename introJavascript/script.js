fc5e634a160cd307e4a633354b51de021983a7d2
rintarotajima <rintarotajima@gmail.com>
2024-04-08 18:43:07 +0900

    [add] else

--- introJavascript/script.js
+++ introJavascript/script.js
@@ -1,15 +1,23 @@
 console.log("Javascriptの世界へようこそ");
 console.log("Javascriptの冒険へ行くぞ");
 
-const age = 25;
+// const age = 25;
 
-if (age < 5) {
-    console.log("無料で映画をご覧になれます");
-} else if (age < 12) {
-    console.log("500円で映画をご覧になれます");
-} else if (age < 15) {
-    console.log("700円で映画をご覧になれます");
-} else if (age >= 20) {
-    console.log("映画を見ることはできません");
+// if (age < 5) {
+//     console.log("無料で映画をご覧になれます");
+// } else if (age < 12) {
+//     console.log("500円で映画をご覧になれます");
+// } else if (age < 15) {
+//     console.log("700円で映画をご覧になれます");
+// } else {
+//     console.log("あなたは年齢制限によって映画をご覧になれません");
+// }
+
+const dayOfWeek = prompt("曜日を英語で入力してください").toLowerCase();
+
+if (dayOfWeek === 'monday') {
+    console.log("今日はプロ野球の試合がない");
+} else {
+    console.log("今日はプロ野球の試合がある");
 }
 
