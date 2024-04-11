82f27436f00ff655be9a46ff60f94d5a6abad0c7
rintarotajima <rintarotajima@gmail.com>
2024-04-11 12:01:56 +0900

    [add] for,whileループ

--- introJavascript/script.js
+++ introJavascript/script.js
@@ -1,5 +1,5 @@
-console.log("Javascriptの世界へようこそ");
-console.log("Javascriptの冒険へ行くぞ");
+// console.log("Javascriptの世界へようこそ");
+// console.log("Javascriptの冒険へ行くぞ");
 
 // const age = 25;
 
@@ -35,54 +35,75 @@ console.log("Javascriptの冒険へ行くぞ");
 //     }
 // }
 
-const playerScore = 120;
-const playTime = 60;
-const levelCleared = true;
+// const playerScore = 120;
+// const playTime = 60;
+// const levelCleared = true;
 
-if (playerScore >= 100 && playTime >= 60 && levelCleared) {
-    console.log("特別なアイテムを獲得しました");
-} else {
-    console.log("条件を満たしていません");
-}
+// if (playerScore >= 100 && playTime >= 60 && levelCleared) {
+//     console.log("特別なアイテムを獲得しました");
+// } else {
+//     console.log("条件を満たしていません");
+// }
 
-const playedScore = 80;
-const playingTime = 30;
-const levelClear = false;
+// const playedScore = 80;
+// const playingTime = 30;
+// const levelClear = false;
 
-if (playedScore >= 100 || playingTime <= 60 || levelClear) {
-    console.log("特別なアイテムを獲得しました");
-} else {
-    console.log("条件を満たしていません");
-}
+// if (playedScore >= 100 || playingTime <= 60 || levelClear) {
+//     console.log("特別なアイテムを獲得しました");
+// } else {
+//     console.log("条件を満たしていません");
+// }
 
-// ユーザの年齢と住所
-const userAge = 17;
-const userCountry = "Japan";
+// // ユーザの年齢と住所
+// const userAge = 17;
+// const userCountry = "Japan";
 
-if (!(userAge >= 18 && userCountry === "Japan")) {
-    console.log("特定のメッセージを表示します");
-} else {
-    console.log("条件を満たしていません");
-}
+// if (!(userAge >= 18 && userCountry === "Japan")) {
+//     console.log("特定のメッセージを表示します");
+// } else {
+//     console.log("条件を満たしていません");
+// }
 
 
 //　ユーザからの入力を取得
-let userInput = prompt("1から4の数字を入力してください");
+// let userInput = prompt("1から4の数字を入力してください");
 
-switch(userInput) {
-    case "1":
-        alert("あなたは1を選択しました");
-        break;
-    case "2":
-        alert("あなたは2を選択しました");
-        break;
-    case "3":
-        alert("あなたは3を選択しました");
-        break;
-    case "4":
-        alert("あなたは4を選択しました");
-        break;
-    default:
-        alert("無効な数字です，1から4の数字を入力してください");
+// switch(userInput) {
+//     case "1":
+//         alert("あなたは1を選択しました");
+//         break;
+//     case "2":
+//         alert("あなたは2を選択しました");
+//         break;
+//     case "3":
+//         alert("あなたは3を選択しました");
+//         break;
+//     case "4":
+//         alert("あなたは4を選択しました");
+//         break;
+//     default:
+//         alert("無効な数字です，1から4の数字を入力してください");
+// }
+
+// for (i = 1; i <= 10; i = i + 1) {
+//     console.log(i);
+// }
+
+// for (i = 2; i <= 20; i = i + 2) {
+//     console.log(i);
+// }
+
+let count = 0;
+while ( count < 10) {
+    count++;
+    console.log(count);
 }
 
+const SECRET = 'superchat';
+
+let guess = '';
+while ( guess !== SECRET ) {
+    guess = prompt("秘密のコードを入力してください");
+}
+console.log("正解,おめでとう!");
