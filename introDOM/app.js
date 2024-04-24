f98fef5d4e7422114819253c057aa3aa6774c5a0
rintarotajima <rintarotajima@gmail.com>
2024-04-24 15:19:32 +0900

    [add] DOM要素の取得

--- introDOM/app.js
+++ introDOM/app.js
@@ -1,2 +1,8 @@
 // DOM要素を取得する
 const heading = document.getElementById("heading"); //getElementById：HTML内の要素をid属性値で取得することができるメソッド
+
+const paragraphs = document.getElementsByTagName('p');  // getElementsByTagName：HTML内の要素を要素名で取得するメソッド
+console.log(paragraphs); 
+
+const introElements = document.getElementsByClassName('intro'); // getElementsByClassName：HTML内の要素をクラス名で取得するメソッド
+console.log(introElements);
