0b24ae913c5660b1925068a32a0dde73e6bd7307
rintarotajima <rintarotajima@gmail.com>
2024-04-30 17:08:33 +0900

    [update] ボタンのテキスト

--- eventDOM/table.js
+++ eventDOM/table.js
@@ -54,7 +54,7 @@ function reset() {
    for (let p of [p1, p2]) {
     p.score = 0;
     p.display.textContent = 0;
-    p.display.classList.remove('has-text-success', 'has-text-danger');
+    p.display.classList.remove('has-text-white', 'has-text-danger');
     p.button.disabled = false;
    }
 }
