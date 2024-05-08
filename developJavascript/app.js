0e6070a33b152832d343b165d5f19860e1e4b049
rintarotajima <rintarotajima@gmail.com>
2024-05-08 14:06:16 +0900

    [add] 自作Promise

--- developJavascript/app.js
+++ developJavascript/app.js
@@ -395,7 +395,13 @@ const colorChange = (color, delay) => {
     })
 }
 
-colorChange('red', 1000);
+colorChange('red', 1000)
+    .then(() => colorChange('orange', 1000))
+    .then(() => colorChange('yellow', 1000))
+    .then(() => colorChange('green', 1000))
+    .then(() => colorChange('blue', 1000))
+    .then(() => colorChange('indigo', 1000))
+    
 
 // awaitキーワード
 // async function rainbow() {
