78ad959b1ab63ae9686b03732f799cf86822f2fc
rintarotajima <rintarotajima@gmail.com>
2024-05-09 23:14:09 +0900

    [add] AxiosでHTTPヘッダー設定

--- axios/app.js
+++ axios/app.js
@@ -16,4 +16,29 @@ const getPerson = async (id) => {
     }
 }
 
+const jokes = document.querySelector('#jokes');
+
+const getDadJoke = async () => {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    const res =  await axios.get('https://icanhazdadjoke.com/', config)
+    return res.data.joke;
+}
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLi = document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi);
+
+}
+
+const button = document.querySelector('#button');
+button.addEventListener('click', addNewJoke)
+
+
 
