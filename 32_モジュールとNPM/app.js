16994dd66d4c6cd669b299acecd2f0617097e8ff
rintarotajima <rintarotajima@gmail.com>
2024-06-29 17:50:12 +0900

    [add] ディレクトリをrequire

--- 32_モジュールとNPM/app.js
+++ 32_モジュールとNPM/app.js
@@ -2,3 +2,6 @@ const math = require('./math.js');
 console.log(math.add(3,5));
 console.log(math.PI);
 console.log(math.square(5))
+
+const allCats = require('./shelter');
+console.log(allCats);
---
+++ 32_モジュールとNPM/shelter/index.js
@@ -0,0 +1,6 @@
+const tora = require('./tora.js');
+const tama = require('./tama.js');
+const momo = require('./momo.js');
+
+const allCats = [tora, tama, momo];
+module.exports = allCats;
