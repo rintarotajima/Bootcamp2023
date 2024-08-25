e50481afa04ea3e5c04b352376d5096d027523a0
rintarotajima <rintarotajima@gmail.com>
2024-08-25 23:51:49 +0900

    [add] 326-言語識別アプリを作ってみる

---
+++ 32_モジュールとNPM/LangGuesser/index.js
@@ -0,0 +1,13 @@
+const franc = require('franc')
+const langs = require('langs')
+const colors = require('colors')
+
+const input = process.argv[2];
+
+const langCode = franc(input);
+if (langCode === 'und') {
+    console.log("解析できませんでした。違う文章でもう1度お試しください。".red)
+} else {
+    const language = langs.where("3", langCode)
+    console.log(`「${language.name}」でしょうか？`.green)
+}
